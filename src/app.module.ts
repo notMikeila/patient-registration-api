0f23963a1d8f9b5f48576a0ae8f82cd7c37bf82b
mi-kei-la <micaela.percovich@gmail.com>
2025-10-01 22:48:41 -0300

    feat: add rate limiting and security headers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { MessagingModule } from './messaging/messaging.module'
-import { PatientModule } from './patient/patient.module'
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { MessagingModule } from "./messaging/messaging.module";
+import { PatientModule } from "./patient/patient.module";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 
 @Module({
   controllers: [AppController],
-  imports: [MessagingModule, PatientModule],
+
+  imports: [
+    ConfigModule.forRoot({ // load environment variables
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([{ // rate limiting config
+      ttl: 60000,
+      limit: 10,
+    }]),
+    MessagingModule,
+    PatientModule,
+  ],
+
+  providers: [
+    {
+      provide: "APP_GUARD",
+      useClass: ThrottlerGuard, // rate limiting guard
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  // add helmet for request security
-  // app.use(helmet())
+  app.use(helmet())
 
   // enable CORS
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT ?? 8080)
+  const port = process.env.PORT || 8080
+  await app.listen(port)
+  Logger.log(`Application running on: ${process.env.PORT}`)
 }
 bootstrap()
