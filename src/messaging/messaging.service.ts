dc1fb147e7cce516df6f9da26c00786b4a276811
mi-kei-la <micaela.percovich@gmail.com>
2025-10-02 01:41:55 -0300

    refactor: improve mailing service instantiation

--- src/messaging/messaging.service.ts
+++ src/messaging/messaging.service.ts
@@ -1,45 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common'
 import {
   TestAccount,
   Transporter,
   createTestAccount,
   createTransport,
-} from 'nodemailer';
+} from 'nodemailer'
 
 @Injectable()
-export class MessagingService {
-  private account: TestAccount;
-  private mailService: Transporter;
+export class EmailService {
+  private account: TestAccount
+  private mailService: Transporter
 
-  private async createTestAccount() {
-    if (!this.account) {
-      await createTestAccount((err, account) => {
-        if (err) {
-          console.error('Failed to create a testing account. ' + err.message);
-          return;
-        }
-        this.account = account;
-        console.log(account);
-      });
-    } else {
-      return this.account;
-    }
+  constructor() {
+    this.initializeService()
   }
 
-  private async createTransport() {
-    if (!this.mailService) {
-      await this.createTestAccount();
-      this.mailService = createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-          user: this.account.user,
-          pass: this.account.pass,
-        },
-      });
-    } else {
-      return this.mailService;
-    }
+  private initializeService() {
+    this.createTestAccount()
+      .then(() => {
+        this.createTransport()
+      })
+      .catch((error) => {
+        Logger.error(
+          `Failed to initialize email service: ${JSON.stringify(error)}`
+        )
+      })
+  }
+
+  private createTestAccount(): Promise<TestAccount> {
+    return new Promise((resolve, reject) => {
+      if (!this.account) {
+        createTestAccount((err, account) => {
+          if (err) {
+            Logger.error(`Failed to create a testing account. ${err.message}`)
+            reject(new InternalServerErrorException(err))
+            return
+          }
+          this.account = account
+          resolve(account)
+        })
+      } else {
+        resolve(this.account)
+      }
+    })
+  }
+
+  private createTransportInstance() {
+    this.mailService = createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: this.account.user,
+        pass: this.account.pass,
+      },
+    })
+    return this.mailService
+  }
+
+  private createTransport(): Promise<Transporter> {
+    return new Promise((resolve, reject) => {
+      if (!this.mailService) {
+        if (!this.account) {
+          this.createTestAccount()
+            .then(this.createTransportInstance)
+            .catch(reject)
+        } else {
+          this.createTransportInstance()
+        }
+      } else {
+        resolve(this.mailService)
+      }
+    })
   }
 
   async sendEmail(
@@ -47,31 +82,28 @@ export class MessagingService {
     name?: string,
     subject?: string,
     text?: string,
-    htmlBody?: string,
-  ) {
+    htmlBody?: string
+  ): Promise<void> {
     const message = {
       from: 'Your Trusted App <info@appdomain.com>',
       to: name ? `${name} <${email}>` : email,
       subject: subject,
-    };
-
-    if (htmlBody) {
-      message['html'] = htmlBody;
-    } else if (text) {
-      message['text'] = text;
-    } else {
-      console.error('No email body provided');
-      return;
     }
 
-    await this.createTransport();
+    if (htmlBody) {
+      message['html'] = htmlBody
+    } else if (text) {
+      message['text'] = text
+    } else {
+      Logger.error('No email body provided')
+      throw new Error('No email body provided')
+    }
 
-    this.mailService.sendMail(message, (err, info) => {
+    await this.mailService.sendMail(message, (err, info) => {
       if (err) {
-        console.error(`Error sending email: ${JSON.stringify(err.message)}`);
-        return;
+        Logger.error(`Error sending email: ${JSON.stringify(err.message)}`)
       }
-      console.log(`Message correctly sent to ${email}`);
-    });
+      Logger.log(`Message correctly sent to ${email}`)
+    })
   }
 }
